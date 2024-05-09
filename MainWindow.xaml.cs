fa2a666e9f2335e3ac7d964f3e87ee20ad9569d0
Charles Zhang <szinubuntu@gmail.com>
2024-05-09 08:52:07 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Base64ClipboardConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                BitmapSource source = Clipboard.GetImage();
+                ImageControl.Source = source;
+            }
+        }
+        private static BitmapImage ConvertToBigmapImage(BitmapSource bitmapSource)
+        {
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            MemoryStream memoryStream = new MemoryStream();
+            BitmapImage image = new BitmapImage();
+
+            encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+            encoder.Save(memoryStream);
+
+            memoryStream.Position = 0;
+            image.BeginInit();
+            image.StreamSource = memoryStream;
+            image.EndInit();
+
+            memoryStream.Close();
+            return image;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Base64 Clipboard Converter
+
+This tool converts an image from clipboard to Base64 format, suitable to be embedded in Markdown.
+
+The implementation of such a tool requires some kind of desktop API support because clipboard is an operating system feature. On the other hand, the general conversion process can be done in just a CLI program.
