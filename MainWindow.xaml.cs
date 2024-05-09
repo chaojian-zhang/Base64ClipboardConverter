4f1a53c5162696a14ad990ef1d221f17c73c0274
Charles Zhang <szinubuntu@gmail.com>
2024-05-09 09:15:49 +0800

    Implement Base64 conversion and text box

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,27 +6,44 @@ using System.Windows.Media.Imaging;
 
 namespace Base64ClipboardConverter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
 
+        #region Events
         private void Window_Activated(object sender, EventArgs e)
         {
             if (Clipboard.ContainsImage())
-                ImageControl.Source = ImageFromClipboardDib();
-        }
+            {
+                // Get image source
+                ImageSource image = BitmapFrame.Create(ImageFromClipboardDib());
 
+                MemoryStream stream = ImageFromClipboardDib()!;
+                byte[] bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+                string base64 = "data:image/jpg;base64," + Convert.ToBase64String(bytes);
+
+                // Set image
+                ImageControl.Source = image;
+                // Set text
+                TextBoxControl.Text = base64;
+            }
+        }
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(TextBoxControl.Text);
+        }
+        #endregion
 
         #region Utility Helper
-        private ImageSource ImageFromClipboardDib()
+        private MemoryStream? ImageFromClipboardDib()
         {
-            MemoryStream ms = Clipboard.GetData("DeviceIndependentBitmap") as MemoryStream;
+            MemoryStream? ms = Clipboard.GetData("DeviceIndependentBitmap") as MemoryStream;
             if (ms != null)
             {
                 byte[] dibBuffer = new byte[ms.Length];
@@ -47,12 +64,11 @@ namespace Base64ClipboardConverter
 
                 byte[] fileHeaderBytes = BinaryStructConverter.ToByteArray<BITMAPFILEHEADER>(fileHeader);
 
-                MemoryStream msBitmap = new MemoryStream();
+                MemoryStream msBitmap = new();
                 msBitmap.Write(fileHeaderBytes, 0, fileHeaderSize);
                 msBitmap.Write(dibBuffer, 0, dibBuffer.Length);
                 msBitmap.Seek(0, SeekOrigin.Begin);
-
-                return BitmapFrame.Create(msBitmap);
+                return msBitmap;
             }
             return null;
         }
